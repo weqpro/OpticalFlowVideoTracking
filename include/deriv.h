6c8e51f6ddcd2bebe6bf4f75504aee7623ae7d3a
weqpro <weqpro@gmail.com>
2026-04-03 12:45:10 +0300

    created files and minor refactor

---
+++ include/deriv.h
@@ -0,0 +1,31 @@
+
+// Copyright 2026 Konovalenko Stanislav and Hombosh Oleh
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DERIV_H
+#define DERIV_H
+
+#include <Eigen/Dense>
+
+namespace vision {
+
+using MatrixXu = Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic>;
+
+void convolve1d(const Eigen::MatrixXd& in_matrix, Eigen::MatrixXd& out_matrix) {
+
+}
+
+}
+
+#endif // !DERIV_H
