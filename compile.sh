c4b13bcc0046b028e597df21fd595edf71f3195e
weqpro <weqpro@gmail.com>
2026-04-28 13:52:29 +0300

    added --test option to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,6 @@
 mkdir -p build
-cmake -B build -DENABLE_CLANG_TIDY=ON && cmake --build build
+cmake -B build -DENABLE_CLANG_TIDY=ON && cmake --build build || exit 1
+
+if [[ "$*" == *"--test"* ]]; then
+    ctest --test-dir build --output-on-failure
+fi
